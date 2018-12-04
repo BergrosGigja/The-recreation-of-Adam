afec8bb38b8282d594bc72fb22da7a38c099d5b6
irisdogg <iriss16@ru.is>
2018-12-04 17:02:47 +0000

    Added TextMeshPro and UIController

---
+++ TheRecreationOfAdam/Assets/Scripts/SpriteOnHover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteOnHover : MonoBehaviour {
+
+    SpriteRenderer m_SpriteRenderer;
+    Color m_MouseOverColor = Color.gray;
+    Color m_OriginalColor;
+    bool isClicked = false;
+
+    UIController _UIController;
+
+    void Start()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        m_OriginalColor = m_SpriteRenderer.color;
+        _UIController = FindObjectOfType<UIController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "Book") {
+                    _UIController.OnBookClicked();
+                }
+            }
+        }
+    }
+
+    void OnMouseOver()
+    {
+        m_SpriteRenderer.color = m_MouseOverColor;
+    }
+
+    void OnMouseExit()
+    {
+        m_SpriteRenderer.color = m_OriginalColor;
+    }
+}
--- TheRecreationOfAdam/Assets/Scripts/UIController.cs
+++ TheRecreationOfAdam/Assets/Scripts/UIController.cs
@@ -1,44 +1,56 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelChanger : MonoBehaviour {
+public class UIController : MonoBehaviour {
 
+    
     public Animator animator;
-
     private int levelToLoad;
 
+    public GameObject book;
+    public GameObject objectInfoText;
+
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if(Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit))
             {
                 if (hit.transform.name == "Interactive") FadeToNextLevel();
             }
         }
-		
-	}
+    }
 
-    public void FadeToNextLevel ()
+    public void FadeToNextLevel()
     {
         Debug.Log("Your active scene is: " + SceneManager.GetActiveScene().buildIndex);
         FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void FadeToLevel (int levelIndex)
+    public void FadeToLevel(int levelIndex)
     {
         levelToLoad = levelIndex;
         Debug.Log("Your scene to load is: " + levelToLoad);
         animator.SetTrigger("FadeOut");
     }
 
-    public void OnFadeComplete ()
+    public void OnFadeComplete()
     {
         Debug.Log("Fade has completed");
 
         SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void OnBookClicked()
+    {
+        Destroy(book);
+        Debug.Log("Book has been destroyed");
+        objectInfoText.SetActive(true);
     }
 }
