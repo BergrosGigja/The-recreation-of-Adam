9accb32c48177a4d48e3654ffb4ab4adef89968e
irisdogg <iriss16@ru.is>
2018-12-06 14:28:08 +0000

    Game controller file added

--- TheRecreationOfAdam/Assets/Scripts/GameController.cs
+++ TheRecreationOfAdam/Assets/Scripts/GameController.cs
@@ -2,19 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController1 : MonoBehaviour {
 
-    public GameObject AdamPrefab;
-
-    float _startPositionX = 5;
-    float _startPositionY = 3;
-
-    // Use this for initialization
-    void Awake()
-    {
-        GameObject Adam = (GameObject)Instantiate(AdamPrefab);
-        Adam.transform.position = new Vector2(_startPositionX, _startPositionY);
-    }
+	// Use this for initialization
+	void Start () {
+		
+	}
 	
 	// Update is called once per frame
 	void Update () {
