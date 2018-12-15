a3a92a65f079e9a6b2b34f37b781f66733f70a57
Anna Lára <annasig95@gmail.com>
2018-12-15 03:34:28 +0000

    Text befor game begins

---
+++ TheRecreationOfAdam/Assets/Scripts/DialogEntry.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogEntry {
+	[TextArea(3,15)]
+	public string[] sentences;
+}
